1650d00b51a66d7ec43f3f3a37073dbd3de50d44
codewizard017 <soumisahu17@gmail.com>
2026-07-12 15:49:07 +0530

    logout button added

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -8,3 +8,33 @@ const sidebarItems = [
   "Analytics",
   "Settings"
 ];
+
+const user =
+JSON.parse(localStorage.getItem("user"));
+
+const profileRole =
+document.getElementById("profileRole");
+
+if(user && profileRole){
+
+    profileRole.textContent =
+    user.role;
+
+}
+
+const logoutBtn =
+document.getElementById("logoutBtn");
+
+if(logoutBtn){
+
+    logoutBtn.addEventListener("click",()=>{
+
+        localStorage.removeItem("user");
+
+        window.location.href =
+        "index.html";
+
+    });
+
+}
+
