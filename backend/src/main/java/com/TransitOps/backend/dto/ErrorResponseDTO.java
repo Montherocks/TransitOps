74d0f0a1579918a7d5d9c25c9deefceaf570fa32
CyberPokemon <imoncoding@gmail.com>
2026-07-12 11:46:06 +0530

    error handling added

---
+++ backend/src/main/java/com/TransitOps/backend/dto/ErrorResponseDTO.java
@@ -0,0 +1,27 @@
+package com.TransitOps.backend.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseDTO {
+
+    private LocalDateTime timestamp;
+
+    private Integer status;
+
+    private String error;
+
+    private String message;
+
+    private String path;
+
+    private Map<String,String> validationErrors;
+
+}
