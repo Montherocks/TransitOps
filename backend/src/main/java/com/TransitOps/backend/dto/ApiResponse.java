dd6ee71243e874fb7da311d001831cb9918c3a23
CyberPokemon <imoncoding@gmail.com>
2026-07-12 11:37:30 +0530

    driver controller created

---
+++ backend/src/main/java/com/TransitOps/backend/dto/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.TransitOps.backend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse<T> {
+
+    private boolean success;
+
+    private String message;
+
+    private T data;
+
+}
